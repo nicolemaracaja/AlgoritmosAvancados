073a44c0a9b50055fc97ce13920b22b5325b1f9c
Nicole Brito Maracajá <139410383+nicolemaracaja@users.noreply.github.com>
2024-06-10 14:25:18 -0300

    Create radioRepeteco.py
    https://vjudge.net/contest/611466/problemPrint/D

---
+++ radioRepeteco.py
@@ -0,0 +1,18 @@
+n = int(input())
+playlist = [int (x) for x in input().split()]
+
+inicio = 0
+comprimento_maximo = 0
+conjunto_musicas = set()  #evita repeticoes
+
+for i in range(n):
+    if playlist[i] not in conjunto_musicas:
+        conjunto_musicas.add(playlist[i])
+        comprimento_maximo = max(comprimento_maximo, i - inicio + 1)
+    else:
+        while playlist[inicio] != playlist[i]:
+            conjunto_musicas.remove(playlist[inicio])
+            inicio += 1
+        inicio += 1
+
+print(comprimento_maximo)

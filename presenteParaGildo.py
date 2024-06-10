3ddb3cf095247bc804be0e8834d0074373aac79a
Nicole Brito Maracajá <139410383+nicolemaracaja@users.noreply.github.com>
2024-06-10 14:23:37 -0300

    Create presenteParaGildo.py
    https://vjudge.net/contest/611466/problemPrint/B

---
+++ presenteParaGildo.py
@@ -0,0 +1,17 @@
+entrada = input()
+saida = list(entrada) #inicia com uma lista dos caracteres da entrada
+tamanho = len(entrada)
+
+for i in range(tamanho - 2, -1, -1):
+    saida[i] = min(saida[i], saida[i + 1])
+
+saida.append('z')
+pilha = []
+resultado = ""
+
+for i in range(tamanho):
+    pilha.append(entrada[i])
+    while pilha and pilha[-1] <= saida[i + 1]:
+        resultado += pilha.pop()
+
+print(resultado)

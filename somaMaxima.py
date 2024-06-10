b39ec8de192b098d156a3844ba740c849b53d495
Nicole Brito Maracajá <139410383+nicolemaracaja@users.noreply.github.com>
2024-06-10 14:18:58 -0300

    Create somaMaxima.py
    https://vjudge.net/contest/607951/problemPrint/E

---
+++ somaMaxima.py
@@ -0,0 +1,12 @@
+n = int(input())  
+seq = list(map(int, input().split())) 
+
+soma_max = float('-inf') 
+soma_atual = 0 
+
+for i in range(n):
+    soma_atual += seq[i]  
+    soma_max = max(soma_max, soma_atual)  
+    soma_atual = max(soma_atual, 0)  #reinicia a soma atual para zero se ela se tornar negativa
+
+print(soma_max)  

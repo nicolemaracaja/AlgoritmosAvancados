a3670a35efb08aa4f6a6695810ddeebb766e8d37
Nicole Brito Maracajá <139410383+nicolemaracaja@users.noreply.github.com>
2024-06-10 14:39:16 -0300

    Create coprimalizando.py
    https://vjudge.net/contest/614541/problemPrint/C

---
+++ coprimalizando.py
@@ -0,0 +1,20 @@
+import math
+
+tamanho = int(input())
+array = list(map(int, input().split()))
+saida = []
+cont = 0
+
+for i in range(tamanho):
+    if i > 0 and math.gcd(array[i-1], array[i]) != 1:
+        saida.append(1)
+        cont += 1
+    saida.append(array[i])
+
+print(cont)
+
+resposta = ''
+for elem in saida:
+    resposta += f' {elem}'
+
+print(resposta.strip())
